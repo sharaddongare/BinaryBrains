f1d9725f5bc1071dfe9bc996de028be2d357002a
nilesh <nil@btr-india.co.in>
2023-08-18 16:27:58 +0530

    Added API

--- src/main/java/com/qa/factory/DriverFactory.java
+++ src/main/java/com/qa/factory/DriverFactory.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
@@ -28,6 +29,9 @@ public class DriverFactory {
         } else if (browser.equals("firefox")) {
             WebDriverManager.firefoxdriver().setup();
             tlDriver.set(new FirefoxDriver());
+        } else if (browser.equals("edge")) {
+            WebDriverManager.edgedriver().setup();
+            tlDriver.set(new EdgeDriver());
         } else if (browser.equals("safari")) {
             tlDriver.set(new SafariDriver());
         } else {
@@ -43,7 +47,6 @@ public class DriverFactory {
 
     }
     public static WebDriver getDriver() {
-
         return tlDriver.get();
     }
 }
