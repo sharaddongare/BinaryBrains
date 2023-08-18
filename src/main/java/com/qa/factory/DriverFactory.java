6b325b01e2585ca3a3f706f3c30c3326b9459647
sharaddongare <dongaresharad@gmail.com>
2023-08-18 19:45:17 +0530

    driver factory changes

--- src/main/java/com/qa/factory/DriverFactory.java
+++ src/main/java/com/qa/factory/DriverFactory.java
@@ -4,10 +4,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 ;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.tools.picocli.CommandLine;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
 
 
 public class DriverFactory {
@@ -21,28 +24,42 @@ public class DriverFactory {
 
         System.out.println("browser value is: " + browser);
 
-        if (browser.equals("chrome")) {
+        if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
             tlDriver.set(new ChromeDriver());
-        } else if (browser.equals("firefox")) {
+            logger.info("Chrome Driver Initiated");
+        } else if (browser.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
             tlDriver.set(new FirefoxDriver());
-        } else if (browser.equals("safari")) {
+            logger.info("Firefox Driver Initiated");
+        } else if (browser.equalsIgnoreCase("safari")) {
             tlDriver.set(new SafariDriver());
+            logger.info("Chrome Driver Initiated");
         } else {
             System.out.println("Please pass the correct browser value: " + browser);
         }
 
         getDriver().manage().deleteAllCookies();
+        logger.info("All cockies Deleted");
         getDriver().manage().window().maximize();
-        logger.info("Webdriver Created");
-        logger.error("Webdriver Created");
-        logger.warn("Webdriver Created");
+        logger.info("Driver Maximized");
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        logger.info("Implicit wait appllied");
         return getDriver();
-
     }
     public static WebDriver getDriver() {
 
         return tlDriver.get();
+    }
+
+    public void closeBrowser(){
+        try {
+            getDriver().close();
+        }
+        catch (Exception e){
+            logger.info("Issue while closing browser");
+        }
+
+
     }
 }
